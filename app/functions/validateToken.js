a9128faab3a299357d3ddcad234469fe8b4adc42
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-09-19 12:01:12 -0400

    Delete app/functions directory

--- app/functions/validateToken.js
+++
@@ -1,9 +0,0 @@
-import { decode } from "jsonwebtoken";
-const validateToken = (token)=>{
-    const isTokenValidate = decode(token);
-    if(isTokenValidate){
-        return true
-    }
-    return true;
-}   
-export {validateToken};
