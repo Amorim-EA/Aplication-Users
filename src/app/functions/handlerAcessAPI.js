540909d4e50dd18ad6ae490b685dbbeeda7b43c2
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-11-30 17:46:49 -0400

    consertei o endpoint do getOneUser handlerAcessAPI.js

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -29,7 +29,7 @@ const getUsers = async () => {
 
 const getOneUser = async (id) => {
   try{
-    const responseOfAPI = await fetch(`${url}/user/${id}`, {
+    const responseOfAPI = await fetch(`${url}/user`, {
       method: "POST",
       cache: "no-cache",
       headers: { "Content-Type": "Application/json" },
